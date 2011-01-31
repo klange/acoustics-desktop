88da37b4ad6eb16f88e82c28342479bc7be6fb1a
Kevin Lange <kevin.lange@dakko.us>
2011-01-30 18:09:41 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Acoustics Desktop #
+
+This is a collection of scripts and tools to make Acoustics interface well with desktop PCs. There's not all that much here, but if I'm bored I might write a new script or tool or widgit or something every so often.
+
+## amp-notification ##
+
+Checks every five seconds for the currently playing song on an Acoustics instance and, if the song changes, displays a notification popup with Notify OSD or similar systems.
---
+++ amp-notification.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import pynotify
+import simplejson, pycurl
+import sys, urllib, time
+
+amp_url = "http://localhost:8080/amp/json.pl"
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+class CurlReader:
+    def __init__(self):
+        self.contents = ''
+    def body_callback(self, buf):
+        self.contents = self.contents + buf
+def curl(url):
+    t = CurlReader()
+    c = pycurl.Curl()
+    c.setopt(c.URL, url)
+    c.setopt(c.WRITEFUNCTION, t.body_callback)
+    c.perform()
+    c.close()
+    return t.contents
+
+pynotify.init("Acoustics")
+
+last_result = -1
+
+while 1:
+    json_output = curl(amp_url)
+    acoustics = simplejson.loads(json_output)
+
+    if acoustics['now_playing']:
+        if not acoustics['now_playing']['song_id'] == last_result:
+            last_result = acoustics['now_playing']['song_id']
+            song_title  = acoustics['now_playing']['title']
+            song_artist = acoustics['now_playing']['artist']
+            song_album  = acoustics['now_playing']['album']
+            n = pynotify.Notification(song_title, "%s<br />%s" % (song_artist, song_album), "notification-audio-volume-high")
+            n.show()
+
+    time.sleep(5)
