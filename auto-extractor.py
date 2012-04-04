e84d5d6a00bd31832693b15b43c1b649fa83c21c
Kevin Lange <kevin.lange@dakko.us>
2012-04-03 19:05:24 -0500

    New stuff

---
+++ auto-extractor.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import simplejson, pycurl
+import sys, urllib, time, os, getopt
+
+from extract_art import extractArt
+
+# Reload `sys` so we can get at the default encoding and set it
+# to unicode (utf-8) so that foreign song titles, etc. work.
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+# The URL of the acoustics instance
+amp_url    = "https://www-s.acm.uiuc.edu/acoustics/json.pl"
+room       = ""
+no_loop    = False
+
+# Argument parsing
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "s:r:n", ["server=", "room=", "no-loop"])
+except getopt.GetoptError, err:
+    print str(err)
+    sys.exit(1)
+for o, a in opts:
+    if o in ("-s", "--server"):
+        amp_url = a + "/json.pl"
+    if o in ("-r", "--room"):
+        room = a
+    if o in ("-n", "--no-loop"):
+        no_loop = True
+
+def getPath(additional):
+    if len(room) > 0:
+        return amp_url + "?player_id=" + room + ";" + additional
+    else:
+        return amp_url + "?" + additional
+
+# Time to sleep between polls
+sleep_time = 5
+
+def curl(url):
+    """ Retreive the contents of the requested url"""
+    f = urllib.urlopen(url)
+    x = f.read()
+    f.close()
+    return x
+
+# The contents of our last poll
+last_result = -1
+
+# Count the number of failed attempts.
+attempts = 1
+
+while 1:
+    try:
+        # Retreive the JSON from the API
+        acoustics = simplejson.loads(curl(getPath("")))
+        # Check that a song is playing
+        if acoustics['now_playing']:
+            # If so, check if this isn't the same data as the last time we polled
+            if not acoustics['now_playing']['song_id'] == last_result:
+                # And if it isn't, build a notification bubble.
+                last_result = acoustics['now_playing']['song_id']
+                song_title  = acoustics['now_playing']['title']
+                song_artist = acoustics['now_playing']['artist']
+                song_album  = acoustics['now_playing']['album']
+                # Print the currently playing song to stdout. Makes a nice log of what's been playing.
+                print "Now Playing: %s\nby: %s\nfrom: %s\n" % (song_title, song_artist, song_album)
+                extractArt(acoustics['now_playing']['path'])
+        else:
+            # Otherwise, nothing is playing.
+            if last_result != 0:
+                print "Nothing playing.\n"
+                last_result = 0
+        # Reset the number of failed attempts.
+        attempts = 1
+    except:
+        # An exception is probably a failed connection or something to that effect
+        print "Failed to connect or retrieve stuff, attempt",attempts
+        # Up the failure count
+        attempts += 1
+        if attempts > 10:
+            # If the failure count is too high, give up and bail.
+            print "Failed to get information after ten attempts. Good bye."
+            sys.exit(1)
+    if no_loop:
+        sys.exit(0)
+    # Wait to poll again
+    time.sleep(sleep_time)
---
+++ extract_art.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from mutagen import File
+import sys, os
+
+oNameJPG = "acoustics-art.jpg"
+oNamePNG = "acoustics-art.png"
+
+def writeData(obj):
+	print "\033[1;32mFound!\033[0m"
+	if obj.mime.find("jpeg") > -1:
+		oName = oNameJPG
+	elif obj.mime.find("png") > -1:
+		oName = oNamePNG
+	else:
+		print "Unknown filetype: ", t.mime
+		return 1
+	f = open(oName,'w')
+	f.write(obj.data)
+	f.close()
+	return 0
+
+def extractArt(fName):
+	global oNameJPG, oNamePNG
+	oNameJPG = os.path.join(os.path.dirname(fName),"acoustics-art.jpg")
+	oNamePNG = os.path.join(os.path.dirname(fName),"acoustics-art.png")
+	print "Extracting art from \033[1;34m%s\033[0m..." % fName,
+
+	fObj  = File(fName)
+
+	if "pictures" in dir(fObj):
+		print "Checking pictures...",
+		if len(fObj.pictures) > 0:
+			return writeData(fObj.pictures[0])
+	else:
+		print "Checking tags...",
+		if "APIC:" in fObj.tags:
+			return writeData(fObj.tags["APIC:"])
+	print "\033[1;31mNothing.\033[0m"
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print "Need a file."
+		sys.exit(1)
+	extractArt(sys.argv[1])
+	sys.exit(2)
